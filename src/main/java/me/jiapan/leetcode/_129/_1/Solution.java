f7d7dc6c636c568ef507e671e8050f4425507bbc
jiapan <panjia@classup.com>
2024-06-03 09:25:41 +0800

    20240603

---
+++ src/main/java/me/jiapan/leetcode/_129/_1/Solution.java
@@ -0,0 +1,37 @@
+package me.jiapan.leetcode._129._1;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int preSum) {
+        if (root == null) {
+            return 0; // 0
+        }
+        int sum = preSum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        }
+        return dfs(root.left, sum) + dfs(root.right, sum);
+    }
+}
