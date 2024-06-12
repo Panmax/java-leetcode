0851d176e838b6b8e117ec110e1cfced07560f6c
jiapan <panjia@classup.com>
2024-06-12 14:44:57 +0800

    20240612

---
+++ src/main/java/me/jiapan/leetcode/_160/_20240611/Solution.java
@@ -0,0 +1,33 @@
+package me.jiapan.leetcode._160._20240611;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<>();
+        ListNode temp = headA;
+        while (temp != null) {
+            visited.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+}
