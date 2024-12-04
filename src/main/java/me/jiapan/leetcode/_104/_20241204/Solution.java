51feec488e26fcdb931f862f912faae9a7bd0466
jiapan <panjia@classup.com>
2024-12-04 11:31:32 +0800

    20241204

---
+++ src/main/java/me/jiapan/leetcode/_104/_20241204/Solution.java
@@ -0,0 +1,32 @@
+package me.jiapan.leetcode._104._20241204;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
