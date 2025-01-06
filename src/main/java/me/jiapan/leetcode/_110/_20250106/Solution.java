379c9820f7f53fe50c492ce8ddf00c8a11a651d2
jiapan <panjia@classup.com>
2025-01-06 17:21:42 +0800

    20250106

---
+++ src/main/java/me/jiapan/leetcode/_110/_20250106/Solution.java
@@ -0,0 +1,36 @@
+package me.jiapan.leetcode._110._20250106;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return Math.max(height(node.left), height(node.right)) + 1;
+    }
+}
