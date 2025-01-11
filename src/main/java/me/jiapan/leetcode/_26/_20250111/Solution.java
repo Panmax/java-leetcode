500b23c4ab8ec1b5fa5b2ac303f58f8d9532eb50
jiapan <panjia@classup.com>
2025-01-11 15:12:21 +0800

    20250111

---
+++ src/main/java/me/jiapan/leetcode/_26/_20250111/Solution.java
@@ -0,0 +1,19 @@
+package me.jiapan.leetcode._26._20250111;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int slow = 1, fast = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
