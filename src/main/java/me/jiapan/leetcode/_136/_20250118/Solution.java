4dd6cdc478cca818fc8428db26f5048947886f74
jiapan <panjia@classup.com>
2025-01-18 11:36:53 +0800

    20250118

---
+++ src/main/java/me/jiapan/leetcode/_136/_20250118/Solution.java
@@ -0,0 +1,11 @@
+package me.jiapan.leetcode._136._20250118;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for (int num : nums) {
+            single ^= num;
+        }
+        return single;
+    }
+}
