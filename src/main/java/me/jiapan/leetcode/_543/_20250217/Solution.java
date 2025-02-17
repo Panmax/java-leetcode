fab010706352c9f4e34dcb9b0a98e5f47ec5af01
jiapan <panjia@classup.com>
2025-02-17 11:23:36 +0800

    20250217

---
+++ src/main/java/me/jiapan/leetcode/_543/_20250217/Solution.java
@@ -0,0 +1,39 @@
+package me.jiapan.leetcode._543._20250217;
+
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    int ans = 1;
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return ans - 1;
+    }
+
+    private int depth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int l = depth(node.left);
+        int r = depth(node.right);
+        ans = Math.max(ans, l + r + 1);
+        return Math.max(l, r) + 1;
+    }
+
+}
