4595f176f605f1bf447e121da05fbe5619a15b89
jiapan <panjia@classup.com>
2025-02-19 14:59:40 +0800

    20250219

---
+++ src/main/java/me/jiapan/leetcode/_114/_20250219/Solution.java
@@ -0,0 +1,46 @@
+package me.jiapan.leetcode._114._20250219;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public void flatten(TreeNode root) {
+        List<TreeNode> list = new ArrayList<>();
+        preOrder(root, list);
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode pre = list.get(i - 1);
+            TreeNode cur = list.get(i);
+            pre.left = null;
+            pre.right = cur;
+        }
+    }
+
+    private void preOrder(TreeNode root, List<TreeNode> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root);
+        preOrder(root.left, list);
+        preOrder(root.right, list);
+    }
+}
