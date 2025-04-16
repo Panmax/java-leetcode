a9116c7b18d4faaf6178061e9293b97537f7a593
jiapan <panjia@classup.com>
2025-04-16 12:01:38 +0800

    20250416

---
+++ src/main/java/me/jiapan/leetcode/_518/_20250416/Solution.java
@@ -0,0 +1,14 @@
+package me.jiapan.leetcode._518._20250416;
+
+public class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
